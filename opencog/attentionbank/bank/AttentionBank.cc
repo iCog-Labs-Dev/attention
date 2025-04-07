19aefaa69fafed6d36b06b55d0b46b1e8370ad12
tarik <tarikmisgana@gmail.com>
2025-04-07 16:48:36 +0300

    WARentcollection-test

--- opencog/attentionbank/bank/AttentionBank.cc
+++ opencog/attentionbank/bank/AttentionBank.cc
@@ -42,7 +42,7 @@ AttentionBank::AttentionBank(AtomSpace* asp)
     targetSTI = 1000000;
     STIAtomWage = 10;
     LTIAtomWage = 10;
-    maxAFSize = 2;
+    maxAFSize = 100;
 
     _as = asp;
     _remove_signal = &asp->atomRemovedSignal();
