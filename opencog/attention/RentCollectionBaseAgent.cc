b5223626fc39bf6d6db90d20f4c34b9be2e01732
tarik <tarikmisgana@gmail.com>
2025-04-08 07:48:57 +0300

    AFRent-similar-test-with metta

--- opencog/attention/RentCollectionBaseAgent.cc
+++ opencog/attention/RentCollectionBaseAgent.cc
@@ -54,33 +54,26 @@ void RentCollectionBaseAgent::load_params(void)
 AttentionValue::sti_t RentCollectionBaseAgent::calculate_STI_Rent()
 {
     double funds = _bank->getSTIFunds();
-    double diff  = 200000- funds;
+    double diff  = targetSTI - funds;
 
-    std::cout << "Funds: " << funds << ", Target STI: " << targetSTI << ", Diff: " << diff << std::endl;
-
-    if(diff <= 0) {
-        std::cout << "Returning 0 (Funds are greater than or equal to target)" << std::endl;
+    if(diff <= 0)
         return 0;
-    }
 
     double ndiff = diff / stiFundsBuffer;
     ndiff = std::min(ndiff, 1.0);
     ndiff = std::max(ndiff, -0.99);
     AttentionValue::sti_t res = STIAtomRent + (STIAtomRent * ndiff);
 
-    std::cout << "Calculated STIRent: " <<res<<"diff "<<ndiff<<"STI atom rent "<<STIAtomRent<<std::endl;
     return res;
 }
-
 
 AttentionValue::sti_t RentCollectionBaseAgent::calculate_LTI_Rent()
 {
     double funds = _bank->getLTIFunds();
-    double diff  = 200000- funds;
+    double diff  = targetLTI - funds;
     double ndiff = diff / ltiFundsBuffer;
     ndiff = std::min(ndiff, 1.0);
     ndiff = std::max(ndiff, -1.0);
 
-    std::cout << "Calculated LTIRent: "<< LTIAtomRent + (LTIAtomRent * ndiff) <<ndiff<<"STI atom rent "<<LTIAtomRent<< std::endl;
     return LTIAtomRent + (LTIAtomRent * ndiff);
 }
